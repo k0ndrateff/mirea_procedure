8fff3159b9bfbcf3a00247ce5ab1f8def0374c53
k0ndrateff <kondratievegor04@yandex.ru>
2022-09-26 22:59:43 +0300

    Add DZ3 (1 and 2)

---
+++ dz3/01_Credit/01_Credit.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "russian");
+    double monthly, summ, r, years, procent;
+
+    cout << "Введите сумму займа: ";
+    cin >> summ;
+    cout << "Введите срок займа в годах: ";
+    cin >> years;
+    cout << "Введите годовой процент займа: ";
+    cin >> procent;
+
+    r = procent / 100;
+    monthly = (summ * r * pow(1 + r, years)) / (12 * (pow(1 + r, years) - 1));
+    cout << "Месячная выплата: " << monthly;
+}
+
---
+++ dz3/02_Loan/02_Loan.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "russian");
+    double monthly, summ, r, years, procent;
+
+    cout << "Введите сумму займа: ";
+    cin >> summ;
+    cout << "Введите срок займа в годах: ";
+    cin >> years;
+    cout << "Введите месячную выплату: ";
+    cin >> monthly;
+
+    if (monthly > 0) {
+        for (int procent = 0; procent < 10000; procent++) {
+            r = procent / 100;
+            if (monthly >= (summ * r * pow(1 + r, years)) / (12 * (pow(1 + r, years) - 1))) {
+                cout << "Процент: " << procent;
+                break;
+            }
+        }
+    }
+    else {
+        for (int procent = -1; procent > -10000; procent--) {
+            r = procent / 100;
+            if (monthly <= (summ * r * pow(1 + r, years)) / (12 * (pow(1 + r, years) - 1))) {
+                cout << "Процент: " << procent;
+                break;
+            }
+        }
+    }
+
+    
+}
+
